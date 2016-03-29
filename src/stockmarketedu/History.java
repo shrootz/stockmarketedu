bb5ef1de035b44aa7f09d5b555c4b8d34a9074bf
shrootz <shruti.subramanian@utexas.edu>
2016-03-28 21:36:04 -0500

    Basic history functionality to store position data

--- src/stockmarketedu/History.java
+++ src/stockmarketedu/History.java
@@ -6,7 +6,32 @@ public class History {
 	private String stockSymbol;
 	private Date dateBought;
 	private Date dateSold;
-	private float shares;
-	private float priceBought;
-	private float priceSold;
+	private double shares;
+	private double priceBought; //perShare
+	private double priceSold; //perShare
+	
+	public History(String stockSymbol, Date dateBought,double shares, double priceBought){
+		this.stockSymbol = stockSymbol;
+		this.dateBought = dateBought;
+		this.shares = shares;
+		this.priceBought = priceBought;
+	}
+	
+	public void buy(double shares, double priceBought){
+		double totalExpenditure = this.shares * this.priceBought;
+		totalExpenditure = totalExpenditure + shares*priceBought;
+		this.shares = this.shares + shares;
+		this.priceBought = totalExpenditure/(this.shares);
+	}
+	
+	public void sell(double shares, double priceSold){
+		double totalExpenditure = this.shares * this.priceSold;
+		totalExpenditure = totalExpenditure + shares*priceSold;
+		this.shares = this.shares - shares;
+		this.priceSold = totalExpenditure/(this.shares);
+	}
+	
+	public String getStockSymbol(){
+		return stockSymbol;
+	}
 }
