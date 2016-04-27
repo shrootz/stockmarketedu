970f5edfd38b4e717f2d0898c715915f045a0467
Sneha <snehashrotriya@utexas.edu>
2016-04-26 22:17:52 -0500

    Ranking Tests

--- src/stockmarketedu/RankStudentsTest.java
+++ src/stockmarketedu/RankStudentsTest.java
@@ -2,9 +2,21 @@ package stockmarketedu;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class RankStudentsTest {
+	private RankByMoney rsbm;
+	private RankByProfitableSale rbps;
+	private RankByProfitPerShare rbpps;
+	private ArrayList<Student> mockStudents;
+	
+	@Before
+	public void setUp(){
+		
+	}
 
 	@Test
 	public void testReturnRanking() {
